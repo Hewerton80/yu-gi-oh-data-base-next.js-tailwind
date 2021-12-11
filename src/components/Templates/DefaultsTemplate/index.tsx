cfe1df4ba05da50dba74c90bee2fe050d6eb3336
hewerton <hewerton80@gmail.com>
2021-12-11 14:36:34 -0300

    add responsive to some components

--- src/components/Templates/DefaultsTemplate/index.tsx
+++ src/components/Templates/DefaultsTemplate/index.tsx
@@ -9,7 +9,7 @@ export function DefaultsTemplate({ children }: DefaultsTemplateProps) {
   return (
     <div className='flex flex-col min-h-screen'>
       <Header/>
-      <main className='flex flex-col px-8'>
+      <main className='flex flex-col px-4 sm:px-8'>
         {children}
       </main>
     </div>
